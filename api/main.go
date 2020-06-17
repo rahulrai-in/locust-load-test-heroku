8b69fe884f7717d5e553f70363275d161ac186a6
Rahul Rai <rahulrai@live.com>
2020-06-17 22:36:20 +1000

    fixed concurrency of request counter
    Signed-off-by: Rahul Rai <rahulrai@live.com>

--- api/main.go
+++ api/main.go
@@ -4,11 +4,12 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"sync/atomic"
 	"time"
 )
 
 func main() {
-	requestCount := 0
+	var requestCount int32 = 0
 
 	portNumber := os.Getenv("PORT")
 
@@ -19,10 +20,11 @@ func main() {
 
 	http.HandleFunc("/volatile", func(w http.ResponseWriter, r *http.Request) {
 		// For every 10 requests, delay the response by 1 second, up to 5 seconds.
-		requestCount++
-		delay := requestCount / 10
+		currentCount := atomic.LoadInt32(&requestCount)
+		atomic.AddInt32(&requestCount, 1)
+		delay := currentCount / 10
 		if delay > 5 {
-			requestCount = 0
+			atomic.StoreInt32(&requestCount, 0)
 			delay = 5
 		}
 
@@ -33,17 +35,15 @@ func main() {
 
 	http.HandleFunc("/buggy", func(w http.ResponseWriter, r *http.Request) {
 		// After every 5 requests, throw error
-		requestCount++
-		count := requestCount
-		delay := count / 10
-		if count%5 == 0 {
-			fmt.Printf("Returning error at %d request \n", count)
+		currentCount := atomic.LoadInt32(&requestCount)
+		atomic.AddInt32(&requestCount, 1)
+		if currentCount%5 == 0 {
+			fmt.Printf("Returning error at %d request \n", currentCount)
 			http.Error(w, http.StatusText(500), 500)
 			return
 		}
 
-		fmt.Fprintf(w, "Ok for request %d", count)
-		fmt.Printf("Produced response after %d second/s \n", delay)
+		fmt.Fprintf(w, "Ok for request %d", currentCount)
 	})
 
 	fmt.Println("Server listening on port ", portNumber)
